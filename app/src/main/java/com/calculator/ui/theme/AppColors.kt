2a7242b451fb2e5f6700de5995226a5686837ed4
Drenae <adrien.guilpain@gmail.com>
2026-09-10 07:42:34 +0200

    Extract application colors

---
+++ app/src/main/java/com/calculator/ui/theme/AppColors.kt
@@ -0,0 +1,15 @@
+package com.calculator.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val AppBackground = Color(0xFF07111E)
+val SurfaceDark = Color(0xFF0E1A2A)
+val SurfaceRaised = Color(0xFF132236)
+val FieldBackground = Color(0xFF101D2C)
+val BorderDark = Color(0xFF2C4058)
+val TextPrimary = Color(0xFFF4F7FB)
+val TextSecondary = Color(0xFF9DB1CC)
+val GreenAccent = Color(0xFF5FE0A7)
+val PurpleAccent = Color(0xFF8C62FF)
+val OrangeAccent = Color(0xFFFFB23F)
+val TextOnLight = Color(0xFF07111E)
